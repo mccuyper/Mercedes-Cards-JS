e93dc3ce5f7dcaab985906597f86c687383a2afc
Alexandr Kononuk <mccuyper@gmail.com>
2021-09-28 16:57:14 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const slides = document.querySelectorAll('.slide')
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses()
+        slide.classList.add('active')
+    })
+}
+
+function clearActiveClasses() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+}
+
+function chBackcolor(color) {
+    document.body.style.background = color;
+ }
